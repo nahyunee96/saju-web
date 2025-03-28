0ea447cae6c177771e5848cf2c0355059d1cca57
nahyunee96 <unique5264@naver.com>
2025-03-28 10:41:45 +0900

    파일정리

--- utilities.js
+++
@@ -1,8 +0,0 @@
-export function pad(num) {
-  return num.toString().padStart(2, '0');
-}
-
-export function setText(id, text) {
-  const elem = document.getElementById(id);
-  if (elem) elem.innerText = text;
-}
